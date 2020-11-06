6864d122095c4591e3aa1749e34d06036bcd454a
hatemeel <staras418@gmail.com>
2020-11-06 10:32:31 +0200

    updated

--- web techs/lab_8/scripts/common.js
+++ web techs/lab_8/scripts/common.js
@@ -1,63 +1,53 @@
-(() => loadXml())();
+(() => {
+  loadXml();
+})();
 
 function loadXml() {
-  $.ajax({
-    url: './data/data.xml',
-    success: (xmlData) => {
-      const parsed = [
-        ...$(xmlData)
-          .find('food')
-          .map((_, food) => {
-            food = $(food);
+  http('./data/data.xml').then((xmlData) => {
+    const parsed = [
+      ...$(xmlData)
+        .find('food')
+        .map((_, food) => {
+          food = $(food);
 
-            return {
-              name: food.find('name').text(),
-              price: food.find('price').text(),
-              description: food.find('description').text(),
-              calories: food.find('calories').text(),
-            };
-          }),
-      ];
+          return {
+            name: food.find('name').text(),
+            price: food.find('price').text(),
+            description: food.find('description').text(),
+            calories: food.find('calories').text(),
+          };
+        }),
+    ];
 
-      $('[data]').html(genList(parsed, 'xml data'));
-    },
+    $('[data]').html(genList(parsed, 'xml data'));
   });
 }
 
 function loadJson(id = 1) {
-  $.ajax({
-    url: `https://jsonplaceholder.typicode.com/todos/${id}`,
-    success: (jsonData) => {
-      $('[data]').html(genList([jsonData], 'json data'));
-    },
+  http(`https://jsonplaceholder.typicode.com/todos/${id}`).then((jsonData) => {
+    $('[data]').html(genList([jsonData], 'json data'));
   });
 }
 
 function loadLnuText() {
-  $.ajax({
-    url: './data/lnu.txt',
-    success: (textData) => {
-      $('[data]').html(`
-				<div class="card card-body">
-					<h3>LNU</h3>
-					<p>${textData}</p>
-				</div>
-			`);
-    },
+  http('./data/lnu.txt').then((textData) => {
+    $('[data]').html(`
+			<div class="card card-body">
+				<h3>LNU</h3>
+				<p>${textData}</p>
+			</div>
+		`);
   });
 }
 
 function loadLvivText() {
-  $.ajax({
-    url: './data/lviv.txt',
-    success: (textData) => {
-      $('[data]').html(`
-				<div class="card card-body">
-					<h3>Lviv</h3>
-					<p>${textData}</p>
-				</div>
-			`);
-    },
+  http('./data/lviv.txt').then((textData) => {
+    $('[data]').html(`
+			<div class="card card-body">
+				<h3>LNU</h3>
+				<p>${textData}</p>
+			</div>
+		`);
   });
 }
 
@@ -80,4 +70,21 @@ function genList(array, title = '') {
   html += '</ul></div>';
 
   return html;
+}
+
+function http(url) {
+  return new Promise((resolve, reject) => {
+    const xml = new XMLHttpRequest();
+    xml.open('get', url, true);
+
+    xml.onload = ({ target: { response } }) => {
+      try {
+        resolve(JSON.parse(response));
+      } catch {}
+      resolve(response);
+    };
+    xml.onerror = reject;
+
+    xml.send();
+  });
 }
